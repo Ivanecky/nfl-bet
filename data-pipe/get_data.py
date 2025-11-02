68a87d57fe70fda1eac69d1ee926982e8df3c1b0
Ivanecky <sam.ivanecky3@gmail.com>
2025-11-02 08:01:14 -0700

    Fixing markets function and converting to polars

--- data-pipe/get_data.py
+++ data-pipe/get_data.py
@@ -9,21 +9,14 @@ import yaml
 # Define the base URL, defined to use the NFL data API
 base_url = "https://api.the-odds-api.com/v4/historical/sports/americanfootball_nfl/"
 
-# Read in API key for Odds API access
-with open(os.getcwd() + '/creds/creds.yaml', 'r') as file:
-    creds = yaml.safe_load(file)
-
-# Define API key
-api_key = creds['paid_api_key']
-
 # Function to define dates
 # Takes in a date in yyyy-MM-dd format and converts it to the proper API format (iso)
-def convert_date(dt: str):
+def convert_date(dt: str) -> str:
     return(dt + 'T23:59:59Z')
 
 
 # Function to get the list of events
-def get_historical_events(api_key: str, date: str):
+def get_historical_events(api_key: str, date: str) -> list:
     # Convert the date to fit api call
     api_dt = convert_date(date)
     # Define the base URL
@@ -41,16 +34,15 @@ def get_historical_events(api_key: str, date: str):
     return event_ids
 
 # Function to extract markets
-def extract_markets(df: pd.DataFrame):
+def extract_markets(market_list: list) -> list:
     # Get each available market for an event and return as a list
-    markets = df['markets']
-    avail_mrkts = list()
-    for i in markets:
-        for k in i:
-            avail_mrkts = avail_mrkts.append(k['key'])
+    avail_mrkts = []
+    for i in market_list:
+        avail_mrkts.append(i['key']) # extracts the key component
+    return(avail_mrkts)
 
 # Function to get the odds from an event ID
-def get_event_markets(api_key:str, event_id: str):
+def get_event_markets(api_key:str, event_id: str) -> pl.DataFrame:
     # Define base url
     base_url = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl"
     # Define the base URL
@@ -63,14 +55,33 @@ def get_event_markets(api_key:str, event_id: str):
     # Extract the data from the API call
     data = mrkt_rqst.json()
     # Get bookmakers
-    bk_df = pd.DataFrame(data['bookmakers'])
+    bk_df = pl.DataFrame(data['bookmakers'])
     # Explode out markets column
-    bk_df['']
+    bk_df = (
+        bk_df.with_columns(
+            pl.col('markets').map_elements(extract_markets, return_dtype=pl.List(pl.Utf8)).alias('market_list')
+        ).drop('markets')
+        .rename({
+         "key": "book_key"
+         , "title": "book_name"   
+        })
+    )
+    # bk_df['market_list'] = bk_df.apply(lambda row: extract_markets(row['markets']), axis=1)
+    # Return dataframe
+    return(bk_df)
 
-
+# Main file
 if __name__ == "__main__":
+    # Read in API key for Odds API access
+    with open(os.getcwd() + '/creds/creds.yaml', 'r') as file:
+        creds = yaml.safe_load(file)
+
+    # Define API key
+    api_key = creds['paid_api_key']
+
     # code test
     test_history = get_historical_events(api_key=api_key, date='2025-10-31')
 
-    # extract information from each event
-
+    # get the markets for an event
+    test_markets = get_event_markets(api_key, test_history[0])
+    
