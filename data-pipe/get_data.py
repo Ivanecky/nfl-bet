7f02e3ebc72a01b61bcb2c39d43b83e9d884929a
Ivanecky <sam.ivanecky3@gmail.com>
2025-11-04 19:49:10 -0700

    Updating functions

--- data-pipe/get_data.py
+++ data-pipe/get_data.py
@@ -1,28 +1,23 @@
 # Import libraries
-import pandas as pd
 import requests as r
-from io import StringIO
-import os
 import polars as pl
-import yaml
 
 # Define the base URL, defined to use the NFL data API
-base_url = "https://api.the-odds-api.com/v4/historical/sports/americanfootball_nfl/"
+base_url = "https://api.the-odds-api.com/"
 
 # Function to define dates
 # Takes in a date in yyyy-MM-dd format and converts it to the proper API format (iso)
 def convert_date(dt: str) -> str:
     return(dt + 'T23:59:59Z')
 
-
 # Function to get the list of events
 def get_historical_events(api_key: str, date: str) -> list:
     # Convert the date to fit api call
     api_dt = convert_date(date)
     # Define the base URL
-    base_url = "https://api.the-odds-api.com/v4/historical/sports/americanfootball_nfl"
+    url = base_url + "v4/historical/sports/americanfootball_nfl"
     # Format API call
-    api_call = f"{base_url}/events?apiKey={api_key}&date={api_dt}"
+    api_call = f"{url}/events?apiKey={api_key}&date={api_dt}"
     # Make API call
     event_rqst = r.get(api_call)
     # Check if the request was successful
@@ -44,9 +39,9 @@ def extract_markets(market_list: list) -> list:
 # Function to get the odds from an event ID
 def get_event_markets(api_key:str, event_id: str) -> pl.DataFrame:
     # Define base url
-    base_url = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl"
+    url = base_url + "v4/sports/americanfootball_nfl"
     # Define the base URL
-    mrkt_url =  base_url + f'/events/{event_id}/markets?apiKey={api_key}&regions=us'
+    mrkt_url =  url + f'/events/{event_id}/markets?apiKey={api_key}&regions=us'
     # Make API call
     mrkt_rqst = r.get(mrkt_url)
     # Check if the request was successful
@@ -70,18 +65,59 @@ def get_event_markets(api_key:str, event_id: str) -> pl.DataFrame:
     # Return dataframe
     return(bk_df)
 
-# Main file
-if __name__ == "__main__":
-    # Read in API key for Odds API access
-    with open(os.getcwd() + '/creds/creds.yaml', 'r') as file:
-        creds = yaml.safe_load(file)
+# Function to get historical odds
+def get_historical_odds(api_key: str, markets: list, dt: str) -> pl.DataFrame:
+    # format markets arg for api call
+    markets_fmt = ",".join(markets)
+    # convert the date
+    conv_dt = convert_date(dt)
+    # format api call
+    url = base_url + f'v4/historical/sports/americanfootball_nfl/odds?apiKey={api_key}&regions=us&markets={markets_fmt}&date={conv_dt}'
+    # make the API call
+    odds_rqst = r.get(url)
+    # check if request was valid
+    if odds_rqst.status_code != 200:
+        raise Exception(f"API request failed with status code {odds_rqst.status_code}")
+    # Extract the data from the API call
+    data = odds_rqst.json()
+    # Convert to polars
+    df = pl.DataFrame(data['data'])
+    # Subset columns
+    df = df.drop('sport_key', 'sport_title')
+    # Return data
+    return(df)
 
-    # Define API key
-    api_key = creds['paid_api_key']
+# Function to get current available events
+def get_current_events(api_key: str) -> pl.List:
+    # Format URL
+    url = base_url + f"/v4/sports/americanfootball_nfl/events?apiKey={api_key}"
+    # Make API call
+    events_rqst = r.get(url)
+    # Check response
+    if events_rqst.status_code != 200:
+        # Raise exception
+        raise Exception(f"API request failed with status code {events_rqst.status_code}")
+    # Extract data
+    events_df = pl.DataFrame(events_rqst.json())
+    # Remove unnecessary columns
+    events_df = events_df.drop('sport_key', 'sport_title')
+    # Return data
+    return(events_df)
 
-    # code test
-    test_history = get_historical_events(api_key=api_key, date='2025-10-31')
-
-    # get the markets for an event
-    test_markets = get_event_markets(api_key, test_history[0])
-    
+# Function to get the current odds available
+def get_current_odds(api_key: str, markets: list, event_id: str) -> pl.DataFrame:
+    # format markets arg for api call
+    markets_fmt = ",".join(markets)
+    # create url for api call
+    url = base_url + f"/v4/sports/americanfootball_nfl/events/{event_id}/odds?apiKey={api_key}&regions=us&markets={markets_fmt}"
+     # make the API call
+    odds_rqst = r.get(url)
+    # check if request was valid
+    if odds_rqst.status_code != 200:
+        raise Exception(f"API request failed with status code {odds_rqst.status_code}")
+    # Extract the data from the API call
+    df = pl.DataFrame(odds_rqst.json())
+    # Remove excess columns
+    df = df.drop('sport_key', 'sport_title')
+    # Return data
+    return(df)
